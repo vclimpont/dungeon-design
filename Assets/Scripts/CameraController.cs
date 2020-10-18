8cac6d99717cd5a294bc7bc33e54bdd57ed643ae
vclimpont <46527505+vclimpont@users.noreply.github.com>
2020-10-19 00:15:14 +0200

    player movement + camera follow player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+
+    private Vector3 cameraOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = playerTransform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    private float horizontalMovement;
+    private float verticalMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetMovementInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(verticalMovement, rb.velocity.y, horizontalMovement);
+    }
+
+    void SetMovementInputs()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * speed;
+        verticalMovement = Input.GetAxisRaw("Vertical") * speed;
+    }
+}
